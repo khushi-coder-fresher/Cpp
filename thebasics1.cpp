2637a12eceec4ca00f56db7690cd75c2005dcf77
khushi-coder-fresher <khushiverma9a@gmail.com>
2025-04-14 18:47:40 +0530

    Create thebasics1.cpp

---
+++ thebasics1.cpp
@@ -0,0 +1,10 @@
+//add two numbers
+#include <iostream>
+using namespace std;
+int addition(int a,int b){
+  return a+b; }
+int main(){
+  cout<< "the sum of 8 and 9 is:"<<addition(8,9)<<endl;
+  return 0;
+}
+  
